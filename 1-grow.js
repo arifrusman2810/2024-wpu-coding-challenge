4e71e9857b8d6cced5f7b44b423f5c2590e93637
Arif2810 <arif83.ar@gmail.com>
2024-02-19 21:39:06 +0700

    day 1/366

---
+++ 1-grow.js
@@ -0,0 +1,20 @@
+// WPU Coding Challenge
+// 1/366
+// https://www.codewars.com/kata/57f780909f7e8e3183000078
+
+// function grow(x){
+//   let result = x[0];
+//   for(let i = 1; i < x.length; i++){
+//     result *= x[i];
+//   }
+//   return result;
+// }
+
+// function grow(x){
+//   const result = x.reduce((acc, curr) => acc * curr, 1);
+//   return result;
+// }
+
+const grow = (x) => x.reduce((acc, curr) => acc * curr, 1);
+
+console.log(grow([2, 2, 2, 2, 2, 2]));
