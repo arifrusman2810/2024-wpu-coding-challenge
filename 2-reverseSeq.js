6d1f7047f24aa6689c12017f6ed3df56a40468f8
Arif2810 <arif83.ar@gmail.com>
2024-02-19 22:04:30 +0700

    Update desc

--- 2-reverseSeq.js
+++ 2-reverseSeq.js
@@ -1,3 +1,7 @@
+// WPU Coding Challenge
+// 2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+
 // const reverseSeq = n => {
 //   const result = [];
 //   for(let i = n; i >= 1; i--){
