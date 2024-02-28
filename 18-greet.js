dca2933af3eba53955732f0df71dab677ab82a62
Arif2810 <arif83.ar@gmail.com>
2024-02-28 21:15:02 +0700

    Day #18/366

---
+++ 18-greet.js
@@ -0,0 +1,25 @@
+// WPU Coding Challenge
+// 18/366
+// https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+// function greet (name, owner) {
+//   if(name === owner) return 'Hello boss';
+//   if(name !== owner) return 'Hello guest';
+// }
+
+// function greet (name, owner) {
+//   if(name === owner){
+//     return 'Hello boss';
+//   }
+//   else{
+//     return 'Hello guest';
+//   }
+// }
+
+// function greet (name, owner) {
+//   return (name === owner) ? 'Hello boss' : 'Hello guest';
+// }
+
+const greet = (name, owner) => name === owner ? 'Hello boss' : 'Hello guest';
+
+console.log(greet('Daniel', 'Daniel'));
