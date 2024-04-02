823201bc83a3e0ffdd7f957b1735135df66e9e2b
unknown <arifrusman2810.com>
2024-04-02 14:05:06 +0700

    Day 25/366

---
+++ 25-doubleChar.js
@@ -0,0 +1,16 @@
+// WPU Coding Challenge
+// 23/366
+// https://www.codewars.com/kata/56b1f01c247c01db92000076
+
+
+// function doubleChar(str) {
+//   let result = '';
+//   for(let i = 0; i < str.length; i++){
+//     result += str[i] + str[i];
+//   }
+//   return result;
+// }
+
+const doubleChar = (str) => str.split('').map((el) => el + el).join('');
+
+console.log(doubleChar("Adidas"));
