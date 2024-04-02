dc04595b1f734bb794aaa8f5d0d4633813a3b454
unknown <arifrusman2810.com>
2024-04-02 14:37:12 +0700

    Day 26/366

---
+++ 26-updateLight.js
@@ -0,0 +1,36 @@
+// WPU Coding Challenge
+// 23/366
+// https://www.codewars.com/kata/58649884a1659ed6cb000072
+
+// function updateLight(current) {
+//   if(current === 'yellow'){
+//     return 'red';
+//   }
+//   else if(current === 'red'){
+//     return 'green';
+//   }
+//   else{
+//     return 'yellow';
+//   }
+// }
+
+// const updateLight = (current) => current === 'yellow' ? 'red' : current === 'red' ? 'green' : 'yellow';
+
+// function updateLight(current) {
+//   switch(current){
+//     case 'yellow' : return 'red';
+//     case 'red' : return 'green';
+//     case 'green' : return 'yellow';
+//   }
+// }
+
+const rules = {
+  yellow: 'red',
+  red: 'green',
+  green: 'yellow',
+}
+function updateLight(current) {
+  return rules[current];
+}
+
+console.log(updateLight("red"));
